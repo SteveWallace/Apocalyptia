4888c1b4ca5d80953f25d52c496b207fe7edc257
Steve Wallace <logosmonkey@gmail.com>
2011-10-04 18:34:04 -0500

    User Authentication and Sheet Saves
    Added in facebook/password authentication as well as save and update
    system with user associations.

--- app.js
+++ app.js
@@ -14,9 +14,6 @@ var Promise = everyauth.Promise;
 var mongooseAuth = require('./node_modules/mongoose-auth/index.js');
 var ObjectId = mongoose.SchemaTypes.ObjectId;
 
-// additional variables
-var rollresult=0;
-
 // Database Declerations
 var Schema = mongoose.Schema
 var UserSchema = new Schema({});
@@ -39,41 +36,44 @@ var CharacterSheet = new Schema({
     Holds     : {type : String, default : '',      required : false},
     XP        : {type : Number, default : '0',     required : true},
     Advances  : {type : Number, default : '0',     required : true},
-    Owner     : {type : String, default : '',      required: true}
+    Owner     : {type : ObjectId}
 });
 
 var db = mongoose.connect('mongodb://localhost:27017/awdb');
 var Sheet = mongoose.model('CharacterSheet', CharacterSheet);
-var cs = new Sheet();
 
-// add in schema plugins via mongooseAuth
- UserSchema.plugin(mongooseAuth, {
-    everymodule: {
-      everyauth: {
-          User: function () {
-            return User;
-          }
-      }
-    }
-  , facebook: {
-      everyauth: {
+
+UserSchema.plugin(mongooseAuth, {
+   everymodule: {
+       everyauth: {
+         User: function () {
+           return User;
+       }
+     }
+   }
+  , password: {
+            everyauth: {
+                getLoginPath: '/login'
+              , postLoginPath: '/login'
+              , loginView: 'login.jade'
+              , getRegisterPath: '/register'
+              , postRegisterPath: '/register'
+              , registerView: 'register.jade'
+              , loginSuccessRedirect: '/'
+              , registerSuccessRedirect: '/'
+            }
+        }
+ , facebook: {
+    everyauth: {
           myHostname: 'http://localhost:3000'
         , appId: '237795932939775'
         , appSecret: 'bf5a2ec8b30fc33f21da63f794d2859a'
         , redirectPath: '/'
-      }
-    }
+    }    
+  }
 });
- 
-var User = mongoose.model('User', UserSchema);
 
-
-//Dice Roller - This rolls 2d6 and adds the argument, generally one of the stats.
- var RollDie = function(arg){
-  total = 0;
-  total = (Math.floor((Math.random()*6)+1) + Math.floor((Math.random()*6)+1)) + Number(arg);
-  return total;
-  };
+var User = mongoose.model('User', UserSchema); 
 
 // Configuration
 var app = module.exports = express.createServer();
@@ -86,14 +86,17 @@ app.configure(function() {
   app.use(express.cookieParser());
   app.use(express.session({secret: 'fiddler'}));
   app.use(mongooseAuth.middleware());
-  //app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
 
 //Add helper arguments
-mongooseAuth.helpExpress(app);
 
-//everyauth.helpExpress(app);
+app.helpers({
+   title:'Apocalyptia - An Apocalypse World Character Keeper'
+ , scripts: ['https://ajax.googleapis.com/ajax/libs/jquery/1.6.3/jquery.js', 'http://static.ak.fbcdn.net/connect/en_US/core.js', 'js/jquery-ui-1.8.16.custom.min.js']
+});
+
+mongooseAuth.helpExpress(app);
 
 app.configure('development', function() {
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
@@ -105,106 +108,118 @@ app.configure('production', function() {
 
 // Routes
 app.get('/', function(req, res){
-        if(req.user) {
-            res.redirect('/myAW/:' + req.user.fb.id);
-        } else {
-              res.render('index', {
-                locals: { title: 'Lazy AW',
-                scripts: ['https://ajax.googleapis.com/ajax/libs/jquery/1.6.3/jquery.js'],
-                }
-              });
-        }
+        
+      if(req.user) {
+          res.redirect('/myAW/:' + req.user.id);
+      } else {
+            res.render('index');
+      }
+
 });
 
 app.get('/myAW/:id', function(req, res){
-     //user verification stuff?
-      Sheet.find({owner: req.params.id}, ['_id'], function(err, cs) {
+    if (!req.user) {
+        console.log('you have to be logged in to get characters');
+    } else {
+      
+      Sheet.find({Owner: req.user._id}, ['Name', '_id'], function(err, cs) {
+      
       if (!err){
-               for (i in cs)
-        {
-          console.log(cs[i]._id);
-        }
-        res.render('index', {
-            locals: { title: 'Lazy AW',
-              scripts: ['https://ajax.googleapis.com/ajax/libs/jquery/1.6.3/jquery.js'],
-              name: cs
-             }
-               });
-             }
-      else {throw err;}
+          
+          for (i in cs)
+          {
+           console.log(cs[i]._id);
+          }
 
-});
+          res.render('index', { locals: { cs: cs }
+        });
+      } else {throw err;}
+
+      });
+    }
 });
 
-app.post('/', function(req, res){
+app.post('/', function(req, res){   
+    res.render('index', { locals: { title: cs._id + 'posted'}});
+});
 
-    //save
-    cs._id   = req.body.Name;
-    cs.Looks = req.body.Looks;
-    cs.Cool  = req.body.Cool;
-    cs.Hot   = req.body.Hot;
-    cs.Sharp = req.body.Sharp;
-    cs.Hard  = req.body.Hard;
-    cs.Weird = req.body.Weird;
-    cs.Owner = req.user.fb.id;
-    cs.save(function (err) {
-      if (!err) console.log('Success!');
-    });
+
+app.get('/sheet/:id', function(req, res) {
+     
+     Sheet.findById(req.params.id, function (err, doc){
+        res.json(doc);
+     });
+});
+
+app.post('/sheet/:csId', function(req, res) {
+// Take the input and update record
+    var keyId = req.params.csId;
+    var owner = req.user._id;
     
-    res.render('index', { locals: { title: cs._id + 'posted', scripts: ['https://ajax.googleapis.com/ajax/libs/jquery/1.6.3/jquery.js']}});
-});
+    
+    if (!req.user._id) {
+      console.log('you must login to save characters');
+      res.end();
+    }
+    console.log('after userId if keyId is ' + keyId);
+    if (keyId === 'new') {
+                  console.log('in id 0 save pre cs def');
+                  var cs = new Sheet();
+                  cs.Name   = req.body.Name;
+                  cs.Looks  = req.body.Looks;
+                  cs.Cool   = req.body.Cool;
+                  cs.Hot    = req.body.Hot;
+                  cs.Sharp  = req.body.Sharp;
+                  cs.Hard   = req.body.Hard;                  
+                  cs.Weird  = req.body.Weird;
+                  cs.Owner  = owner;  
 
-app.get('/roll', function(req, res){
-    var obj = req.query['value1'];
-    var rollresult = RollDie(obj);
-    console.log(obj);
-    res.json(rollresult);
-    res.end();
-});
-
-app.get('/sheet/:name', function(req, res) {
-     console.log(req.params.name + 'yup');
-     Sheet.findById(req.params.name, function (err, doc){
-      console.log(req.params.name);
-      res.json(doc);
-    });
-});
-
-app.post('/sheet/:name', function(req, res) {
-
-    // Take the input and update record
-    console.log(req.body.Name + req.body.Cool + req.body.Hot + req.body.Looks + 'about to save');
-    var keyId = req.params.name;
-    console.log('key id =' + keyId);
+              cs.save(function (err) {
+                 console.log('save step');
+               if (!err) {
+                 console.log('saved, opening next');
+                 res.end(); 
+               } else {throw err}
+              });  
+    } else {
 
     Sheet.findById(keyId, function (err, cs) {
-      if (!err) {
-    cs.Looks  = req.body.Looks;
-    cs.Cool   = req.body.Cool;
-    cs.Hot    = req.body.Hot;
-    cs.Sharp  = req.body.Sharp;
-    cs.Hard   = req.body.Hard;
-    cs.Weird  = req.body.Weird;
-    cs.Owner  = req.user.fb.id;
-
-     } else {
-      console.log('there was an error');    
-     }
-    cs.save(function (err) {
-     if (!err) {
-     console.log('saved, opening next'); 
-    } else {throw err}
-     });
-   });
-  res.end(); 
-});
-
-app.get('/characters', function(req, res) {
-
-});
-
-app.del('/', function(req, res){
-  
+        if (err) {
+             console.log(err);
+             res.end();
+          } else {
+              if (cs) {
+                  console.log('trying to update');
+                  cs.Name   = req.body.Name;
+                  cs.Looks  = req.body.Looks;
+                  cs.Cool   = req.body.Cool;
+                  cs.Hot    = req.body.Hot;
+                  cs.Sharp  = req.body.Sharp;
+                  cs.Hard   = req.body.Hard;
+                  cs.Weird  = req.body.Weird;
+                  //cs.Owner  = owner;
+              } else {
+                  console.log('trying to save');
+                  var cs = new Sheet();
+                  cs.Name   = req.body.Name;
+                  cs.Looks  = req.body.Looks;
+                  cs.Cool   = req.body.Cool;
+                  cs.Hot    = req.body.Hot;
+                  cs.Sharp  = req.body.Sharp;
+                  cs.Hard   = req.body.Hard;
+                  cs.Weird  = req.body.Weird;
+                  cs.Owner  = owner;  
+               }
+              cs.save(function (err) {
+                console.log('save step');
+               if (!err) {
+                console.log('saved, opening next');
+                res.end(); 
+               } else {throw err}
+              });
+           }  
+    });
+  }
 });
 
 app.listen(3000);
